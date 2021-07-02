7a510267af4f5826dfb98fa31f8b480d88383407
MinjaeLee <2alswo7@khu.ac.kr>
2021-07-03 02:09:03 +0900

    [ADD] #18 - 공통 shadow 추가

--- DooRiBon/DooRiBon/Sources/Main/MainViewController.swift
+++ DooRiBon/DooRiBon/Sources/Main/MainViewController.swift
@@ -8,11 +8,12 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    
 }
