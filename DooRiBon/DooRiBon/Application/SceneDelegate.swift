6e5119a9245c5f7ef266f96b2bf4bc33e005a867
김태현 <taehyk528@gmail.com>
2021-07-06 03:54:55 +0900

    [FEAT] #15 - 탭바 연결 및 UI 세팅 - 탑 쉐도우 적용 - 아이콘 select, unselect 세팅 - 아이템 inset 임의 조정

--- DooRiBon/DooRiBon/Application/SceneDelegate.swift
+++ DooRiBon/DooRiBon/Application/SceneDelegate.swift
@@ -19,11 +19,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window = UIWindow(windowScene: windowScene)
         
         // 맨 처음 보여줄 뷰 컨트롤러 객체 생성 (루트 뷰 컨트롤러로 사용할 객체)
-        let rootViewController = UIStoryboard(name: "MainStoryboard", bundle: nil)
-            .instantiateViewController(identifier: "MainViewController")
+//        let rootViewController = UIStoryboard(name: "MainStoryboard", bundle: nil)
+//            .instantiateViewController(identifier: "MainViewController")
+        let rootViewController = UIStoryboard(name: "TripStoryboard", bundle: nil)
+            .instantiateViewController(identifier: "TripViewController")
         
         // 윈도우 위에 쌓이는 것 중에서 윈도우와 가장 근접한 부분을 rootViewController 라고 함
         window?.rootViewController = UINavigationController(rootViewController: rootViewController)
+
         // 생성한 윈도우를 핵심 윈도우로 보여줌 (윈도우는 여러 개 생성 가능)
         window?.makeKeyAndVisible()
 
