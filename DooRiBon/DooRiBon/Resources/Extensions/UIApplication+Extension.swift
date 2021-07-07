b143734bb682d90c8077b993e7e64514105c4ee5
hyejinL <knca2@naver.com>
2021-07-07 14:33:33 +0900

    [FEAT] #39 - 주석 추가

--- DooRiBon/DooRiBon/Resources/Extensions/UIApplication+Extension.swift
+++ DooRiBon/DooRiBon/Resources/Extensions/UIApplication+Extension.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 extension UIApplication {
+    // MARK: 윈도우를 찾아서 rootViewController 반환
     var rootViewController: UIViewController? {
         guard let windowScene = connectedScenes.first as? UIWindowScene,
               let sceneDelegate = windowScene.delegate as? SceneDelegate else {
@@ -16,6 +17,8 @@ extension UIApplication {
         return sceneDelegate.window?.rootViewController
     }
 
+    // MARK: 받은 viewController에 가장 상위의 presentedViewController를 찾아 반환
+    // - 원하는 viewController가 없을 경우 기본적으로 위에서 찾은 rootViewController를 기준으로 가장 상위의 viewController를 찾음
     class func topViewController(
         _ viewController: UIViewController? = UIApplication.shared.rootViewController
     ) -> UIViewController? {
