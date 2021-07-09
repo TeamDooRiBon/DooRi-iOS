ac890a5d0ebfd6b9a8d339998902f85e677f45ff
김태현 <taehyk528@gmail.com>
2021-07-09 18:15:28 +0900

    [FEAT] #59 - 여행멤버뷰 상단 영역 버튼 액션 추가
    - 뒤로가기
    - 프로필
    - 설정
    - 멤버
    - 참여코드복사
    - 5가지 버튼에 대한 버튼 액션 추가 (addTarget)

--- DooRiBon/DooRiBon/Sources/Base/Board/BoardViewController.swift
+++ DooRiBon/DooRiBon/Sources/Base/Board/BoardViewController.swift
@@ -27,6 +27,7 @@ extension BoardViewController {
         navigationController?.navigationBar.isHidden = true
         
         setupTop()
+        setupButtonAction()
     }
     
     // MARK: - Top Area
@@ -38,5 +39,35 @@ extension BoardViewController {
             $0.width.equalToSuperview()
             $0.height.equalTo(190)
         }
+    }
+    
+    // MARK: - Buttons
+    
+    private func setupButtonAction() {
+        topView.backButton.addTarget(self, action: #selector(backButtonClicked), for: .touchUpInside)
+        topView.profileButton.addTarget(self, action: #selector(profileButtonClicked), for: .touchUpInside)
+        topView.settingButton.addTarget(self, action: #selector(settingButtonClicked), for: .touchUpInside)
+        topView.memberButton.addTarget(self, action: #selector(memberButtonClicked), for: .touchUpInside)
+        topView.codeButton.addTarget(self, action: #selector(codeButtonClicked), for: .touchUpInside)
+    }
+    
+    @objc func backButtonClicked(_ sender: UIButton) {
+        print("back button clicked")
+    }
+    
+    @objc func profileButtonClicked(_ sender: UIButton) {
+        print("profile button clicked")
+    }
+    
+    @objc func settingButtonClicked(_ sender: UIButton) {
+        print("setting button clicked")
+    }
+    
+    @objc func memberButtonClicked(_ sender: UIButton) {
+        print("member button clicked")
+    }
+    
+    @objc func codeButtonClicked(_ sender: UIButton) {
+        print("code button clicked")
     }
 }
