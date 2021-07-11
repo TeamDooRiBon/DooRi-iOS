551387fa27b36cef43ac57b1b03aaf1db13671d9
Hansangjin98 <onetable98@naver.com>
2021-07-12 03:34:08 +0900

    [FEAT] #68 - 초기세팅

---
+++ DooRiBon/DooRiBon/Sources/Base/Member/StyleTestResult/StyleTestResultViewController.swift
@@ -0,0 +1,33 @@
+//
+//  StyleTestResultViewController.swift
+//  DooRiBon
+//
+//  Created by 한상진 on 2021/07/12.
+//
+
+import UIKit
+
+class StyleTestResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
