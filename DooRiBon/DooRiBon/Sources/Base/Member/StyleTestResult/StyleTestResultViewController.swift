4cafa1e0ca626dc238b9db63aca5707d29846206
Hansangjin98 <onetable98@naver.com>
2021-07-12 18:31:49 +0900

    [FEAT] #68 - 성향테스트 결과 뷰 프로토타입 구현

--- DooRiBon/DooRiBon/Sources/Base/Member/StyleTestResult/StyleTestResultViewController.swift
+++ DooRiBon/DooRiBon/Sources/Base/Member/StyleTestResult/StyleTestResultViewController.swift
@@ -6,28 +6,58 @@
 //
 
 import UIKit
+import SnapKit
 
 class StyleTestResultViewController: UIViewController {
 
+    //MARK:- IBOutlet
+    
+    @IBOutlet weak var resultImageView: UIImageView!
+    @IBOutlet weak var backButton: UIButton!
+    
+    //MARK:- Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        imageSet()
+        shadowSet()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.navigationBar.isHidden = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK:- Function
+    
+    func imageSet() {
+        guard let url = URL(string: "https://cdn.cocoacasts.com/cc00ceb0c6bff0d536f25454d50223875d5c79f1/above-the-clouds.jpg") else { return }
+        DispatchQueue.global().async {
+            if let data = try? Data(contentsOf: url) {
+                DispatchQueue.main.async {
+                    if let image = UIImage(data: data) {
+                        self.resultImageView.image = image.resized(to: CGSize(width: UIScreen.main.bounds.width, height: image.size.height))
+                    }
+                }
+            }
+        }
     }
-    */
-
+    
+    func shadowSet() {
+        backButton.layer.applyShadow(color: Colors.black2.color, alpha: 0.08, x: 0, y: 1, blur: 10, spread: 2)
+    }
+    
+}
+extension UIImage {
+    public func resized(to target: CGSize) -> UIImage? {
+        let ratio = min(
+            target.height / size.height, target.width / size.width
+        )
+        let newSize = CGSize(
+            width: size.width * ratio, height: size.height * ratio
+        )
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        draw(in: CGRect(origin: .zero, size: newSize))
+        defer { UIGraphicsEndImageContext() }
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
 }
