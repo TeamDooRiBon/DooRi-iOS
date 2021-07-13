276b9478c5e1af8391ca2346e42bc384bea0f484
김태현 <taehyk528@gmail.com>
2021-07-13 16:31:32 +0900

    [ADD] #81 - 여행 데이터 모델 생성(참여코드뷰)
    - 참여코드에서 맞는 여행정보인지 체크할 때 사용하는 데이터 모델 생성

---
+++ DooRiBon/DooRiBon/Sources/JoinTrip/Models/JoinTripDataModel.swift
@@ -0,0 +1,47 @@
+//
+//  JoinTripDataModel.swift
+//  DooRiBon
+//
+//  Created by taehy.k on 2021/07/13.
+//
+
+/*
+ 여행 정보를 Response로 받아오는데 , 동일한 형태가 있으면 하나로 사용해도 괜찮을 듯
+ {
+     "status": 200,
+     "success": true,
+     "message": "참여 코드로 여행 찾기 성공",
+     "data": {
+         "groupId": "60ebfdfabc1ffc1325a15f22",
+         "travelName": "배고픈 여행",
+         "host": "채정아",
+         "destination": "우리 집",
+         "startDate": "2021-8-21-0:0",
+         "endDate": "2021-8-24-0:0",
+         "image": "https://dooribon.s3.ap-northeast-2.amazonaws.com/1.png"
+     }
+ }
+ 
+ - 네이밍에 대한 고민
+ */
+import Foundation
+
+// MARK: - JoinTrip 전체 데이터 모델
+struct JoinTripDataModel: Codable {
+    let status: Int
+    let success: Bool
+    let message: String
+    let data: JoinTripData?
+}
+
+// MARK: - JoinTrip 데이터
+struct JoinTripData: Codable {
+    let groupID, travelName, host, destination: String
+    let startDate, endDate: String
+    let image: String
+
+    enum CodingKeys: String, CodingKey {
+        case groupID = "groupId"
+        case travelName, host, destination, startDate, endDate, image
+    }
+}
