889b5be023c030aa89e2a3351b75bdc0f66d869e
Hansangjin98 <onetable98@naver.com>
2021-07-14 22:53:27 +0900

    [FEAT] #104 - 성향테스트 가중치 계산 완료

--- DooRiBon/DooRiBon/Sources/Main/StyleTest/StyleQuestionViewController.swift
+++ DooRiBon/DooRiBon/Sources/Main/StyleTest/StyleQuestionViewController.swift
@@ -20,27 +20,23 @@ class StyleQuestionViewController: UIViewController {
     @IBOutlet weak var indicatorBar: UIView!
     
     var questionDataList: [StyleQuestionData] = []
-//    var questionTitleList: [String] = []
     var questionAnswerWeightList: [StyleQuestionAnswerWeight] = []
-//    private lazy var answers: [Int] = Array(repeating: -1, count: answerList.count)
+    private lazy var weightResult: [Int] = Array(repeating: 0, count: 8)
+    private lazy var answers: [Int] = Array(repeating: -1, count: questionDataList.count)
+    var selectedWeight: [Int] = []
     
     
 // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        navigationController?.navigationBar.isHidden = true
-        
-        serAnswerList()
-        
         answerCollectionView.delegate = self
         answerCollectionView.dataSource = self
-
         answerCollectionView.isScrollEnabled = false
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = true
         questionDataListSet()
     }
     
@@ -48,57 +44,63 @@ class StyleQuestionViewController: UIViewController {
 
     // 이전 문항 버튼 클릭시
     @IBAction func previousButtonClicked(_ sender: Any) {
-//        guard let cell = answerCollectionView.visibleCells.first,
-//           let currentNumber = answerCollectionView.indexPath(for: cell)?.item else {
-//            return
-//        }
-//        let previousItem = max(currentNumber - 1, 0)
-//        answerCollectionView.scrollToItem(at: IndexPath(item: previousItem, section: 0), at: .left, animated: true)
-//        updateQuestion(previousItem)
+        guard let cell = answerCollectionView.visibleCells.first,
+           let currentNumber = answerCollectionView.indexPath(for: cell)?.item else {
+            return
+        }
+        let previousItem = max(currentNumber - 1, 0)
+        answerCollectionView.scrollToItem(at: IndexPath(item: previousItem, section: 0), at: .left, animated: true)
+        updateQuestion(previousItem)
+        for i in Range(0...7) {
+            weightResult[i] -= selectedWeight[i]
+        }
+        print("가중치 합 : \(weightResult)")
     }
     
     // 다음 문항 버튼 클릭시
     @IBAction func nextButtonClicked(_ sender: Any) {
-//        guard let cell = answerCollectionView.visibleCells.first,
-//              let currentNumber = answerCollectionView.indexPath(for: cell)?.item,
-//              currentNumber < answerList.count else {
-//            return
-//        }
-//        // 4개 중 1개를 선택할때만 버튼을 작동시킴
-//        if (answers[currentNumber] != -1) {
-//            guard let cell = answerCollectionView.visibleCells.first,
-//                  let currentNumber = answerCollectionView.indexPath(for: cell)?.item else {
-//                return
-//            }
-//            nextButton.backgroundColor = Colors.gray7.color
-//            nextButton.setTitleColor(Colors.gray4.color, for: .normal)
-//
-//            let nextItem = min(currentNumber + 1, answerList.count - 1)
-//            answerCollectionView.scrollToItem(at: IndexPath(item: nextItem, section: 0), at: .left, animated: true)
-//            updateQuestion(nextItem)
-//        }
-//
-//        // 마지막 버튼 이름 바꾸기
-//        if (currentNumber == 8) {
-//            nextButton.setTitle("결과 보러가기", for: .normal)
-//        }
+        guard let cell = answerCollectionView.visibleCells.first,
+              let currentNumber = answerCollectionView.indexPath(for: cell)?.item, currentNumber < questionDataList.count else {
+            return
+        }
+        // 4개 중 1개를 선택할때만 버튼을 작동시킴
+        if (answers[currentNumber] != -1) {
+            guard let cell = answerCollectionView.visibleCells.first,
+                  let currentNumber = answerCollectionView.indexPath(for: cell)?.item else {
+                return
+            }
+            nextButton.backgroundColor = Colors.gray7.color
+            nextButton.setTitleColor(Colors.gray4.color, for: .normal)
+
+            let nextItem = min(currentNumber + 1, questionDataList.count - 1)
+            answerCollectionView.scrollToItem(at: IndexPath(item: nextItem, section: 0), at: .left, animated: true)
+            updateQuestion(nextItem)
+        }
+        for i in Range(0...7) {
+            weightResult[i] += selectedWeight[i]
+        }
+        print("가중치 합 : \(weightResult)")
+        // 마지막 버튼 이름 바꾸기
+        if (currentNumber == 8) {
+            nextButton.setTitle("결과 보러가기", for: .normal)
+        }
     }
     
     // 상단 x버튼 클릭 시
     @IBAction func outTestButtonClicked(_ sender: Any) {
-//        PopupView.loadFromXib()
-//            .setTitle("정말 나가시겠습니까?")
-//            .setDescription("""
-//                            지금까지의 응답 정보는 저장되지 않습니다.
-//                            테스트 중단을 원하신다면 오른쪽 버튼을 눌러주세요
-//                            """)
-//            .setCancelButton("계속할게요")
-//            .setConfirmButton("나갈게요")
-//            .present { event in
-//                if event == .confirm {
-//                    // confirm action
-//                }
-//            }
+        PopupView.loadFromXib()
+            .setTitle("정말 나가시겠습니까?")
+            .setDescription("""
+                            지금까지의 응답 정보는 저장되지 않습니다.
+                            테스트 중단을 원하신다면 오른쪽 버튼을 눌러주세요
+                            """)
+            .setCancelButton("계속할게요")
+            .setConfirmButton("나갈게요")
+            .present { event in
+                if event == .confirm {
+                    // confirm action
+                }
+            }
     }
     
     // MARK: - 함수
@@ -110,13 +112,8 @@ class StyleQuestionViewController: UIViewController {
             case .success(let data):
                 if let titleContent = data as? StyleQuestionResponse {
                     questionDataList = titleContent.data
-//                    questionTitleListSet()
                     answerCollectionView.reloadData()
                     updateQuestion(0)
-                    
-//                    comeTripCollectionView.reloadData()
-//                    lastTripTableView.reloadData()
-//                    setDevideTripData()
                 }
             case .requestErr(let message):
                 print("requestERR", message)
@@ -129,70 +126,12 @@ class StyleQuestionViewController: UIViewController {
             }
         }
     }
-    
-    func questionTitleListSet() {
-//        print("질문 제목 리스트 추가된거 확인")
-//        questionTitleList.append(questionDataList[0].title)
-//        print(questionTitleList)
-//        print("끝")
-//        questionAnswerListSet()
-    }
-    
-    func questionAnswerListSet() {
-//        print("질문 대답 리스트 추가된거 확인")
-//        questionAnswerList.append(questionDataList[0].content[0].answer)
-//        print(questionAnswerList)
-//        print("끝")
-    }
-    
-    // 문제 선택지 리스트
-    func serAnswerList()
-    {
-//        answerList.append(contentsOf: [
-//            AnswerDataModel(answer1: "시간 단위로 세세하게 장소와 동선까지 정하고싶어", answer2: "지역이랑 숙소만 정하고 나머지는 여행 중에 정할래", answer3: "계획 없이 기분에 따라 발길 닿는대로 다니고 싶어", answer4: "함께 하는 사람들이 하자는 대로 할게"),
-//            AnswerDataModel(answer1: "이동 시간은 최대한 줄이고 관광을 더 하고 싶어", answer2: "천천히 이곳저곳 둘러보며 이동할래", answer3: "이동을 많이 하고싶지 않아. 움직이는게 싫어", answer4: "이동도 함께하면 즐거울거야. 무엇이든 상관없어"),
-//            AnswerDataModel(answer1: "최대한 많은 관광지를 둘러보고 싶어", answer2: "쉬엄쉬엄 여유롭게 구경 다니고 싶어", answer3: "관광보다는 편안한 곳에서 느긋하게 힐링하고 싶어", answer4: "같이 가는 사람들이 하자는 대로 할래"),
-//            AnswerDataModel(answer1: "당연히 풍경사진도, 내 인생샷도 열심히 찍어야지", answer2: "주변 사람한테 부탁해서 단체사진부터 찍고싶어", answer3: "함께 간 사람들의 사진을 찍어줄래", answer4: "사진을 찍기보다는 그 순간을 내 눈에 담고 싶어"),
-//            AnswerDataModel(answer1: "미리 다른 식당도 조사해 놓을거라 걱정없어", answer2: "다시 검색해서 검증된 식당을 갈래", answer3: "주변 사람들한테 맛있는 식당을 아는지 물어볼래", answer4: "그 근처에 괜찮아 보이는 식당으로 갈래"),
-//            AnswerDataModel(answer1: "사람이 붐비는 시끌벅적한 곳이야", answer2: "우리밖에 없는 한적하고 조용한 곳이야", answer3: "그날의 기분에 따라 달라질 것 같아", answer4: "다같이 있다면 장소의 분위기 상관없이 어디든 좋아"),
-//            AnswerDataModel(answer1: "나 혼자라도 다녀오고 싶어", answer2: "그룹 일정이 끝나고 혼자 다녀 올래", answer3: "다음을 기약하며 그곳을 포기하고 그룹이랑 다닐래", answer4: "그곳이 얼마나 좋은지 어필해서 그룹을 설득시킬래"),
-//            AnswerDataModel(answer1: "새로운 사람들과 즐겁게 대화를 이어 갈래", answer2: "다른 친구들이 대답하는 걸 지켜보고 있을거야", answer3: "내가 바로 먼저 말을 거는 그 사람이야!", answer4: "모르는 사람을 만나기보다는 우리끼리만 놀고 싶어"),
-//            AnswerDataModel(answer1: "그래도 계획한 만큼만 머물고 다음 장소로 이동할래", answer2: "다음 계획을 위해 떠나고 다른 날에 다시 방문할래", answer3: "기존 계획을 변경하더라도 그곳에 오래 있을래", answer4: "함께 하는 사람들의 의견을 따를래"),
-//            AnswerDataModel(answer1: "사람들이 많이 가는 유명한 장소 위주로 다닐래", answer2: "랜드마크 몇 군데만 가고 나머지는 마음대로 다닐래", answer3: "아무도 안 가본 새로운 장소를 찾아보고 싶어", answer4: "함께 하는 사람들이 가자는 대로 갈래")
-//        ])
-    }
 
     // 질문 내용 변경
     func updateQuestion(_ currentNumber: Int) {
         questionNumberLabel.text = "Q\(currentNumber + 1)"
         questionLabel.text = questionDataList[currentNumber].title
         questionAnswerWeightList = questionDataList[currentNumber].content
-        // 질문 내용 변경
-//        switch currentNumber {
-//        case 0:
-//            self.questionDataList[currentNumber].title
-////            self.questionLabel.text = "계획을 세울 때 나는?"
-//        case 1:
-//            self.questionLabel.text = "한 장소에서 다른 장소로 이동할 때 나는?"
-//        case 2:
-//            self.questionLabel.text = "내가 원하는 여행 스타일은"
-//        case 3:
-//            self.questionLabel.text = "멋진 풍경이 내 눈 앞에 펼쳐졌을 때 나는?"
-//        case 4:
-//            self.questionLabel.text = "가려고 했던 식당이 문을 닫았을 때 나는?"
-//        case 5:
-//            self.questionLabel.text = "내가 더 많은 시간을 보내고 싶은 곳은?"
-//        case 6:
-//            self.questionLabel.text = "일행과 서로 가고 싶은 곳이 다를 때는?"
-//        case 7:
-//            self.questionLabel.text = "일행과 서로 가고 싶은 곳이 다를 때는?"
-//        case 8:
-//            self.questionLabel.text = "다른 사람들이 말을 걸어왔을 때 나는?"
-//        case 9:
-//            self.questionLabel.text = "방문할 장소를 선택할 때 나는?"
-//        default:
-//            self.questionLabel.text = " "
-//        }
     }
 
     // 인디케이터 바
@@ -202,17 +141,17 @@ class StyleQuestionViewController: UIViewController {
     
     // 다음 문항 버튼 색상 변경
     func buttonChangeColor() {
-//        guard let cell = answerCollectionView.visibleCells.first,
-//              let currentNumber = answerCollectionView.indexPath(for: cell)?.item,
-//              currentNumber < answerList.count else {
-//            return
-//        }
-//
-//        if (answers[currentNumber] == 0 || answers[currentNumber] == 1 ||
-//            answers[currentNumber] == 2 || answers[currentNumber] == 3) {
-//            nextButton.backgroundColor = Colors.pointOrange.color
-//            nextButton.setTitleColor(Colors.white9.color, for: .normal)
-//        }
+        guard let cell = answerCollectionView.visibleCells.first,
+              let currentNumber = answerCollectionView.indexPath(for: cell)?.item,
+              currentNumber < questionDataList.count else {
+            return
+        }
+
+        if (answers[currentNumber] == 0 || answers[currentNumber] == 1 ||
+            answers[currentNumber] == 2 || answers[currentNumber] == 3) {
+            nextButton.backgroundColor = Colors.pointOrange.color
+            nextButton.setTitleColor(Colors.white9.color, for: .normal)
+        }
     }
 
 }
@@ -274,13 +213,13 @@ extension StyleQuestionViewController: UICollectionViewDelegateFlowLayout
 extension StyleQuestionViewController: AnswerCollectionViewCellDelegate {
     func didSelectedAnswer(_ index: Int) {
         
-//        guard let cell = answerCollectionView.visibleCells.first,
-//              let currentNumber = answerCollectionView.indexPath(for: cell)?.item,
-//              currentNumber < answerList.count else {
-//            return
-//        }
-//        answers[currentNumber] = index
-//        buttonChangeColor()
-//        print(answers)
+        guard let cell = answerCollectionView.visibleCells.first,
+              let currentNumber = answerCollectionView.indexPath(for: cell)?.item,
+              currentNumber < questionDataList.count else {
+            return
+        }
+        answers[currentNumber] = index
+        buttonChangeColor()
+        selectedWeight = questionAnswerWeightList[index].weight
     }
 }
